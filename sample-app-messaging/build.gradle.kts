ee857c0cbff9803b0f82aa3283edafeda0137c0d
Colin Simmonds <ctsimmonds@avaya.com>
2024-05-16 10:55:37 -0400

    Inline the dependency information and add a couple of missing dependencies.

--- sample-app-messaging/build.gradle.kts
+++ sample-app-messaging/build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
-    alias(libs.plugins.google.services)
-    alias(libs.plugins.ksp)
-    `maven-publish`
+    id("com.android.application") version "8.3.2"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
+    id("com.google.gms.google-services") version "4.4.1"
+    id("com.google.devtools.ksp") version "1.9.22-1.0.18"
 }
-
-val pomVersion = property("pom.version") as String
 
 android {
     namespace = "com.avaya.axp.client.sample_app_messaging"
@@ -19,7 +16,6 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -56,73 +52,37 @@ android {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = providers.gradleProperty("pom.group.id").get()
-            artifactId = "sample-app-messaging"
-            version = pomVersion
-            artifact("build/outputs/apk/release/${artifactId}-release-unsigned.apk")
-            pom {
-                name = "AXP Client SDK - Sample App Messaging"
-                description = "AXP clients support for sample app messaging"
-                // TODO: before really publishing, will need to fill in a license and URL here
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            val releasesRepoUrl = "https://nexus.forge.avaya.com/repository/metam-maven-release/"
-            val snapshotsRepoUrl = "https://nexus.forge.avaya.com/repository/metam-maven-snapshot/"
-            url = uri(
-                if (pomVersion.toString()
-                        .endsWith("SNAPSHOT")
-                ) snapshotsRepoUrl else releasesRepoUrl
-            )
-            credentials {
-                username = if (hasProperty("nexusUsername")) property("nexusUsername") as String else ""
-                password = if (hasProperty("nexusPassword")) property("nexusPassword") as String else ""
-            }
-        }
-    }
-}
-
 dependencies {
-    implementation(project(":core"))
-    implementation(project(":messaging"))
-    implementation(project(":messaging-ui"))
+    implementation("com.avaya.axp.client.sdk:core:0.1.0")
+    implementation("com.avaya.axp.client.sdk:messaging:0.1.0")
+    implementation("com.avaya.axp.client.sdk:messaging-ui:0.1.0")
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(group = "androidx.activity", name = "activity-compose", version = "1.9.0")
+    implementation(group = "androidx.compose.material", name = "material-icons-extended", version = "1.6.7")
+    implementation(group = "androidx.compose.ui", name = "ui-graphics")
+    implementation(group = "androidx.compose.ui", name = "ui-tooling-preview")
+    implementation(group = "androidx.compose.ui", name = "ui")
+    implementation(group = "androidx.compose", name = "compose-bom", version = "2024.05.00")
+    implementation(group = "androidx.core", name = "core-ktx", version = "1.13.1")
+    implementation(group = "androidx.lifecycle", name = "lifecycle-runtime-ktx", version = "2.7.0")
+    implementation(group = "androidx.navigation", name = "navigation-compose", version = "2.7.7")
+    implementation(group = "androidx.work", name = "work-runtime-ktx", version = "2.9.0")
+    implementation(group = "com.google.android.material", name = "material", version = "1.12.0")
 
-    implementation(libs.moshi)
-    implementation(libs.moshi.kotlin)
-    ksp(libs.moshi.codegen)
-    implementation(libs.okhttp.logging)
+    implementation(group = "com.google.firebase", name = "firebase-common-ktx", version = "21.0.0")
+    implementation(group = "com.google.firebase", name = "firebase-bom", version = "33.0.0")
+    implementation(group = "com.google.firebase", name = "firebase-messaging", version = "24.0.0")
 
-    implementation(libs.logback.android)
+    implementation(group = "com.squareup.moshi", name = "moshi", version = "1.15.1")
+    implementation(group = "com.squareup.moshi", name = "moshi-kotlin", version = "1.15.1")
+    ksp(group = "com.squareup.moshi", name = "moshi-kotlin-codegen", version = "1.15.1")
+    implementation(group = "com.squareup.okhttp3", name = "logging-interceptor", version = "4.12.0")
+    implementation(group = "com.squareup.retrofit2", name = "retrofit", version = "2.11.0")
+    implementation(group = "com.squareup.retrofit2", name = "converter-moshi", version = "2.11.0")
 
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.compose.material.iconsext)
-    implementation(libs.okhttp.logging)
+    implementation(group = "com.github.tony19", name = "logback-android", version = "3.0.0")
+    implementation(group = "org.slf4j", name = "slf4j-api", version = "2.0.13")
 
-    implementation(libs.firebase.common.ktx)
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.messaging)
-    implementation(libs.androidx.work)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(group = "androidx.compose.ui", name = "ui-tooling")
+    debugImplementation(group = "androidx.compose.ui", name = "ui-test-manifest", version = "1.6.7")
 }
